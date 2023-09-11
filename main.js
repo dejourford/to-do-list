b57be9c1164d064cdd9ceefd14788f2c605b680e
dejourford <dejourford@my.unt.edu>
2023-09-10 23:08:45 -0500

    define all needed variables

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const addToDoListInput = document.querySelector('.add-to-do-input')
+const addBtn = document.querySelector('.add-Btn')
+const itemCheckbox = document.querySelector('.to-do-item-checkbox')
+const deleteBtn = document.querySelector('.delete-btn')
+
 // CREATE TO-DO-ITEM
 // GET INPUTTED TO-DO-ITEM
 // STORE TO-DO-ITEM
