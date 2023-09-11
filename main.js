37032f9356cbdc93fcb8c4a69272f76c2fbd12ea
dejourford <dejourford@my.unt.edu>
2023-09-10 23:10:24 -0500

    change variable name

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-const addToDoListInput = document.querySelector('.add-to-do-input')
+const ToDoListInput = document.querySelector('.add-to-do-input')
 const addBtn = document.querySelector('.add-Btn')
 const itemCheckbox = document.querySelector('.to-do-item-checkbox')
 const deleteBtn = document.querySelector('.delete-btn')
 
 // CREATE TO-DO-ITEM
+function getToDoItem() {
+
+}
 // GET INPUTTED TO-DO-ITEM
 // STORE TO-DO-ITEM
 // DISPLAY STORED TO-DO-ITEM
