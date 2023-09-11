c82362a78eeb5d3d6a5bf0a19c68343f817813e7
dejourford <dejourford@my.unt.edu>
2023-09-10 23:39:29 -0500

    reset input after submit button pressed

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
-const ToDoListInput = document.querySelector('.add-to-do-input')
+
+
+const toDoListInput = document.querySelector('.add-to-do-input')
 const addBtn = document.querySelector('.add-Btn')
 const itemCheckbox = document.querySelector('.to-do-item-checkbox')
 const deleteBtn = document.querySelector('.delete-btn')
+let toDoItemInput
+
+// GET TO-DO-ITEM
+addBtn.addEventListener('click', function() {
+    console.log('input submitted')
+    toDoItemInput = toDoListInput.value
+    console.log(toDoItemInput)
+    toDoListInput.value = ''
+})
+
+function getToDoItem(toDoItemInput) { 
+    console.log(toDoItemInput)
+    
+    
+    
+}
+
 
 // CREATE TO-DO-ITEM
-function getToDoItem() {
-
+function createToDoItem() {
+    console.log('item created')
+    const toDoItem = getToDoItem()
+    console.log(toDoItem)
 }
-// GET INPUTTED TO-DO-ITEM
-// STORE TO-DO-ITEM
 // DISPLAY STORED TO-DO-ITEM
 // ABILITY TO EDIT/DELETE/SET AS COMPLETED TO-DO-ITEM
 // STORE TO-DO-LIST ITEMS IN LOCAL STORAGE
+
